3685c47f34fdaa8f18f95adacf61413455d1157d
edyatnaputra <rizkyedyatnaputra@gmail.com>
2016-03-27 20:21:04 +0700

    perubahan src

--- src/praktikum4/Bukualamat.java
+++ src/praktikum4/Bukualamat.java
@@ -1,61 +1,109 @@
-package praktikum;
+package praktikum4;
+
+import java.util.Scanner;
 
 public class Bukualamat {
-    
+
+    Scanner in = new Scanner(System.in);
     private String nama;
     private String alamat;
     private String nope;
     private String email;
-    
-    
-    public Bukualamat(){
-        nama="";
-        alamat="";
-        nope="";
-        email="";
+
+    public Bukualamat() {
+        this.nama = "";
+        this.alamat = "";
+        this.nope = "";
+        this.email = "";
     }
-    
-    
+
+    public Bukualamat(String nama, String alamat, String nope, String email) {
+        this.alamat = alamat;
+        this.email = email;
+        this.nama = nama;
+        this.nope = nope;
+    }
+
+    public static void display(Bukualamat[] buku) {
+        int i;
+        for (i = 0; i < buku.length; i++) {
+            
+            if (buku[i].getNama().equals("")) {
+                System.out.println("DATA KE-"+(i + 1)+" DATA TERHAPUS");
+            } else {
+                System.out.println("DATA KE-"+(i + 1));
+                System.out.println("NAMA          : " + buku[i].getNama());
+                System.out.println("ALAMAT        : " + buku[i].getAlamat());
+                System.out.println("NOMER TELEPON : " + buku[i].getNope());
+                System.out.println("EMAIL         : " + buku[i].getEmail());
+            }
+
+        }
+
+    }
+
+    public static void input(Bukualamat[] buku, int i) {
+        buku[i] = new Bukualamat();
+        System.out.print("NAMA          : ");
+        buku[i].setNama();
+        System.out.print("ALAMAT        : ");
+        buku[i].setAlamat();
+        System.out.print("NOMER TELEPON : ");
+        buku[i].setNope();
+        System.out.print("EMAIL         : ");
+        buku[i].setEmail();
+    }
+
+    public static void edit(Bukualamat[] buku, int a) {
+        --a;
+        buku[a] = new Bukualamat();
+        System.out.print("NAMA          : ");
+        buku[a].setNama();
+        System.out.print("ALAMAT        : ");
+        buku[a].setAlamat();
+        System.out.print("NOMER TELEPON : ");
+        buku[a].setNope();
+        System.out.print("EMAIL         : ");
+        buku[a].setEmail();
+
+    }
+
+    public static void delete(Bukualamat[] buku, int a) {
+        --a;
+        buku[a] = new Bukualamat();
+
+    }
 
     public String getNama() {
         return nama;
     }
 
-    
-    public void setNama(String nama) {
-        this.nama = nama;
+    public void setNama() {
+        this.nama = in.nextLine();
     }
 
     public String getAlamat() {
         return alamat;
     }
 
-    public void setAlamat(String alamat) {
-        this.alamat = alamat;
+    public void setAlamat() {
+        this.alamat = in.nextLine();
     }
 
     public String getNope() {
         return nope;
     }
 
-    public void setNope(String nope) {
-        this.nope = nope;
+    public void setNope() {
+        this.nope = in.nextLine();
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setEmail() {
+        this.email = in.nextLine();
     }
 
-    public void display (){
-        System.out.println("Nama      :"+nama);
-        System.out.println("Alamat    :"+alamat);
-        System.out.println("Nope      :"+nope);
-        System.out.println("Email     :"+email);
-        
-    }
-    
 }
--- src/praktikum4/Main.java
+++ src/praktikum4/Main.java
@@ -1,53 +1,58 @@
 package praktikum4;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-       
-        int a =0;
-        Bukualamat m = new Bukualamat (); 
-        
-        Scanner input = new Scanner (System.in);
-        do{
-        System.out.println("1. Masukkan Data");
-        System.out.println("2. Lihat Data");
-        System.out.println("3. Keluar");
-        System.out.print("> ");
-        int pil = input.nextInt();
-        
-        switch (pil){
-            case 1:  
-                System.out.print("Masukkan Nama          :");
-                input.nextLine();
-                String nama = input.nextLine();
-                System.out.print("Masukkan Alamat        :");
-                String alamat = input.nextLine();
-                System.out.print("Masukkan Email         :");
-                String email= input.nextLine();
-                System.out.print("Masukkan Nomor Telepon :");
-                String nope = input.nextLine();
-                m.setNope(nope);
-                m.setNama(nama);
-                m.setAlamat(alamat);
-                m.setEmail(email);
-                break;
-            case 2:
-                m.display();
-                break;
-            case 3:
-                System.out.println("Program Berhenti");
-                a=3;
-                break;
-            default : System.out.println("Inputan Salah");
-                break;
-                
-        }
-                
-                
-        }while (a==0);
-        
+
+        Bukualamat[] buku = new Bukualamat[0];
+        int i = 0, u = 0, w = 0;
+        Scanner input = new Scanner(System.in);
+
+        do {
+            System.out.println("1. Masukkan Data");
+            System.out.println("2. Lihat Data");
+            System.out.println("3. Hapus Data");
+            System.out.println("4. Ubah Data");
+            System.out.println("0. Keluar");
+            System.out.print("> ");
+            int pil = input.nextInt();
+
+            switch (pil) {
+                case 1:
+                    buku = Arrays.copyOf(buku, buku.length + 1);
+                    Bukualamat.input(buku, i);
+                    i++;
+                    break;
+                case 2:
+                    Bukualamat.display(buku);
+                    break;
+                case 3:
+                    Bukualamat.display(buku);
+                    System.out.print("Hapus Data Ke- ? ");
+                    w = input.nextInt();
+                    System.out.print("y/n untuk menghapus");
+                    if (input.next().equals("y") && (w - 1) < buku.length) {
+                        Bukualamat.delete(buku, w);
+                    } 
+                    break;
+                case 4:
+                    Bukualamat.display(buku);
+                    System.out.print("Edit Data Ke- ? ");
+                    w = input.nextInt();
+                    if ((w - 1) < buku.length) {
+                        Bukualamat.edit(buku, w);
+                    }
+                    break;
+                case 0:
+                    u++;
+                    break;
+
+            }
+
+        } while (u == 0);
+
     }
-    
+
 }
